9703602a9ad2ab2458096e99aba46b9c60ee88b7
Sirish-C <schejerl@pnw.edu>
2023-11-27 00:13:38 -0600

    Minor Changes

--- Kruskal_And_Prims/Prims.java
+++ Kruskal_And_Prims/Prims.java
@@ -1,3 +1,4 @@
+
 // A simple Java implementation to find minimum
 // spanning tree for adjacency representation.
 import java.io.BufferedReader;
@@ -5,34 +6,33 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.*;
 
-class Prims
-{
+class Prims {
     static int V = 119;
     static int[][] distance = new int[V][V];
-    static String []  cityMap = new String[V];
+    static String[] cityMap = new String[V];
     static int INT_MAX = Integer.MAX_VALUE;
 
-    public static String getCity(int index){
+    public static String getCity(int index) {
         return cityMap[index];
     }
-    public static void load() throws Exception{
+
+    public static void load() throws Exception {
         // File path is passed as parameter
         File file = new File("./DistanceMS2.csv");
         BufferedReader br = new BufferedReader(new FileReader(file));
         String st;
-        int i =0;
-        while ((st = br.readLine()) != null){
-            String [] arr = st.split(",");
+        int i = 0;
+        while ((st = br.readLine()) != null) {
+            String[] arr = st.split(",");
             cityMap[i] = arr[0];
-            for(int j =1;j<arr.length;j++){
-                distance[i][j-1] = Integer.parseInt(arr[j])==0?INT_MAX:Integer.parseInt(arr[j]);
+            for (int j = 1; j < arr.length; j++) {
+                distance[i][j - 1] = Integer.parseInt(arr[j]) == 0 ? INT_MAX : Integer.parseInt(arr[j]);
             }
             i++;
         }
     }
-    static boolean isValidEdge(int u, int v,
-                               boolean[] inMST)
-    {
+
+    static boolean isValidEdge(int u, int v, boolean[] inMST) {
         if (u == v)
             return false;
         if (inMST[u] == false && inMST[v] == false)
@@ -42,9 +42,8 @@ class Prims
         return true;
     }
 
-    static void primMST(int cost[][])
-    {
-        boolean []inMST = new boolean[V];
+    static void primMST(int cost[][]) {
+        boolean[] inMST = new boolean[V];
 
         // Include first vertex in MST
         inMST[0] = true;
@@ -52,19 +51,14 @@ class Prims
         // Keep adding edges while number of included
         // edges does not become V-1.
         int edge_count = 0, mincost = 0;
-        while (edge_count < V - 1)
-        {
+        while (edge_count < V - 1) {
 
             // Find minimum weight valid edge.
             int min = INT_MAX, a = -1, b = -1;
-            for (int i = 0; i < V; i++)
-            {
-                for (int j = 0; j < V; j++)
-                {
-                    if (cost[i][j] < min)
-                    {
-                        if (isValidEdge(i, j, inMST))
-                        {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    if (cost[i][j] < min) {
+                        if (isValidEdge(i, j, inMST)) {
                             min = cost[i][j];
                             a = i;
                             b = j;
@@ -73,19 +67,20 @@ class Prims
                 }
             }
 
-            if (a != -1 && b != -1)
-            {
-                System.out.printf("Edge %d:(%s, %s) cost: %d \n",
+            if (a != -1 && b != -1) {
+                System.out.printf("Edge %d:(%s, %s) Distance : %d \n",
                         edge_count++, getCity(a), getCity(b), min);
                 mincost = mincost + min;
                 inMST[b] = inMST[a] = true;
             }
         }
-        System.out.printf("\n Minimum Distance  = %d \n  miles∂", mincost);
+        System.out.printf("\n Minimum Distance  = %d miles \n", mincost);
     }
 
     // Driver Code
     public static void PrimsAlgo() throws Exception {
+        System.out.println("Executing Prims Algorithm");
+        System.out.println("============================");
         load();
         // Print the solution
         primMST(distance);
